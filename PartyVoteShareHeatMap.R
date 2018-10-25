5afdc95b83370d02031d12fb14a64cf93ba301ad
sloppyspeech <sloppy.speech@gmail.com>
2018-10-25 17:06:58 +0530

    Update PartyVoteShareHeatMap.R

--- PartyVoteShareHeatMap.R
+++ PartyVoteShareHeatMap.R
@@ -8,11 +8,11 @@ library(vidiris)
 
 res <- read.csv("./Data/PartyVoteShare.csv")
 res1 <- res
-res1$Party <- factor(res1$Party,levels = rev(sort(res1$Party)))
+res1$Party <- factor(res1$Party,levels = unique(sort(res1$Party)))
 View(res1)
 ggplot(data=res1,aes(x=State,y=Party)) +
-+ geom_tile(aes(fill=VoteShare)) +
-+ scale_fill_viridis(option = "plasma") +
-+ theme_solarized_2() +
-+ xlab("State") +
-+ ylab("Party") +guides(fill = guide_legend(title="Vote Share"))
+geom_tile(aes(fill=VoteShare)) +
+scale_fill_viridis(option = "plasma") +
+theme_solarized_2() +
+xlab("State") +
+ylab("Party") +guides(fill = guide_legend(title="Vote Share"))
